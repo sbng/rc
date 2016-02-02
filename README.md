e68e598decaa184428658b15f6eeb96e719968ab
sbng <seo.boon.ng@gmail.com>
2016-02-02 13:43:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# rc
+
+Route compare between OSPF and BGP. Perform a longest prefix match between OSPF and BGP RIB. Missing OSPF routes will be shown
+
+Input: Capture of "show route ospf" and "show ip bgp"
+
+Output: Missing OSPF route
+
+Requirement: Python subnettree module
---
+++ rc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python -tt
+
+# f = open(filename, 'r')
+
+import sys
+import os
+import re
+import SubnetTree
+
+if len(sys.argv) < 2:
+        sys.stderr.write('Usage: rc.py <filename>\nOutput: <file>.diff\n')
+        sys.exit(1)
+else:
+        with open(sys.argv[1], 'r') as f:
+            filename = os.path.splitext(sys.argv[1])[0]
+            data = f.read()
+            
+def main():
+    #sys.stdout = open(filename+'.diff', 'w')
+    ospf_exp = re.compile('O .. .+?(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\/\d\d)')
+    bgp_exp = re.compile('[\>|r|\*|i].+? (\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\/\d\d)')
+    # this RE match those prefixes without "/nn"
+    # IOS do not append "/" for default mask eg /32 in 
+    # 192.89.222.0/24 in BGP will appears as 192.89.222.0
+    bgp_null_exp = re.compile('[\>|r|\*|i]+? +(\d{1,3}.\d{1,3}.\d{1,3}.0) ')
+    ospf = ospf_exp.findall(data)
+    bgp = bgp_exp.findall(data)
+    bgp_null = bgp_null_exp.findall(data)
+
+    # Build a BGP table (Tree)
+    bgp_rib = SubnetTree.SubnetTree()
+    for x in bgp: bgp_rib.insert(x) 
+    for x in bgp_null : bgp_rib.insert(x) 
+  
+    # Check all OSPF prefixes to see if it's in BGP rib
+    # Test the prefix and isert the result into the 
+    # OSPF present database
+    ospf_present = { x: x in bgp_rib for x in ospf }
+
+    for x in ospf_present: 
+        # Only print the missing OSPF if the input is false
+        if (ospf_present[x] == False):
+            print x,":",ospf_present[x]
+
+if __name__ == '__main__':
+  main()
+
